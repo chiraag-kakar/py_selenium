455d2f60214f0f331aeda1023f2d3c189506bd54
Encryptzen <ck2222@cse.jgec.ac.in>
2021-01-24 01:07:57 +0530

    scripts

---
+++ script2.py
@@ -0,0 +1,21 @@
+#Selenium Webdriver must be installed for this to work
+
+import time;
+from selenium import webdriver;
+
+#time to refresh page (seconds)
+Timer = 120
+
+#youtube link
+link = 'https://www.youtube.com/watch?v=pFSJWMmHhfo'
+
+#number of views
+views = 100
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+for i in range(views):
+    time.sleep(Timer)
+    driver.refresh()
+    print(i)
